04ac5d7746cd544a2c82b26cc9ec985051e95fcd
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-26 15:57:55 +0300

    finish Operators task 5 module 3 stage 0

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+     void divide(int toBeDivided) {
+         float result = (float) toBeDivided/5;
+        System.out.println(result);
+
     }
 }
